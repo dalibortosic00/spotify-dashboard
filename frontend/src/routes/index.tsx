80d2eb8105e93f18d2bee024d08de97728d26db6
Dalibor Tošić <dalibortosic00@gmail.com>
2025-09-15 16:51:33 +0200

    add data time range buttons

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,11 +1,13 @@
+import { createFileRoute, redirect } from "@tanstack/react-router";
+import { useState } from "react";
 import FactCard from "../components/FactCard.tsx";
 import TopItemsCard from "../components/TopItemsCard.tsx";
 import { useAuth } from "../hooks/useAuth.ts";
 import { useTopItems } from "../hooks/useTopItems.ts";
 import ChartCard from "../components/ChartCard.tsx";
 import GenreChart from "../components/GenreChart.tsx";
-import { createFileRoute, redirect } from "@tanstack/react-router";
 import { getStoredAuth } from "../utils/auth.ts";
+import type { TimeRange } from "../types.ts";
 
 export const Route = createFileRoute("/")({
   beforeLoad: () => {
@@ -19,6 +21,7 @@ export const Route = createFileRoute("/")({
 });
 
 function DashboardPage() {
+  const [timeRange, setTimeRange] = useState<TimeRange>("medium_term");
   const { token, isCheckingToken } = useAuth();
   const {
     data: topItems,
@@ -26,6 +29,7 @@ function DashboardPage() {
     error,
   } = useTopItems({
     token,
+    params: { time_range: timeRange },
     enabled: !isCheckingToken,
   });
 
@@ -44,50 +48,81 @@ function DashboardPage() {
     const { top_artists, top_tracks } = topItems;
 
     return (
-      <div className="dashboard-grid">
-        <FactCard
-          title="Your Top Artist"
-          item={top_artists.items[0]}
-          icon={
-            <span role="img" aria-label="star">
-              ⭐
-            </span>
-          }
-        />
-
-        <FactCard
-          title="Your Top Track"
-          item={top_tracks.items[0]}
-          icon={
-            <span role="img" aria-label="music note">
-              🎵
-            </span>
-          }
-        />
-
-        {top_artists.items.length > 1 && (
-          <TopItemsCard
-            title="Your Top Artists"
-            items={top_artists.items.slice(0, 10)}
-          />
-        )}
-
-        {top_tracks.items.length > 1 && (
-          <TopItemsCard
-            title="Your Top Tracks"
-            items={top_tracks.items.slice(0, 10)}
-          />
-        )}
-
-        {top_artists.items.length > 1 && (
-          <ChartCard
-            title="Your Top Genres"
-            subtitle="Based on your top artists"
+      <>
+        <div className="time-range-buttons">
+          <button
+            type="button"
+            className={timeRange === "long_term" ? "active" : ""}
+            onClick={() => {
+              setTimeRange("long_term");
+            }}
           >
-            <GenreChart artists={top_artists.items} />
-          </ChartCard>
-        )}
-      </div>
+            Last Year
+          </button>
+          <button
+            type="button"
+            className={timeRange === "medium_term" ? "active" : ""}
+            onClick={() => {
+              setTimeRange("medium_term");
+            }}
+          >
+            Last 6 Months
+          </button>
+          <button
+            type="button"
+            className={timeRange === "short_term" ? "active" : ""}
+            onClick={() => {
+              setTimeRange("short_term");
+            }}
+          >
+            Last 4 Weeks
+          </button>
+        </div>
+        <div className="dashboard-grid">
+          <FactCard
+            title="Your Top Artist"
+            item={top_artists.items[0]}
+            icon={
+              <span role="img" aria-label="star">
+                ⭐
+              </span>
+            }
+          />
+
+          <FactCard
+            title="Your Top Track"
+            item={top_tracks.items[0]}
+            icon={
+              <span role="img" aria-label="music note">
+                🎵
+              </span>
+            }
+          />
+
+          {top_artists.items.length > 1 && (
+            <TopItemsCard
+              title="Your Top Artists"
+              items={top_artists.items.slice(0, 10)}
+            />
+          )}
+
+          {top_tracks.items.length > 1 && (
+            <TopItemsCard
+              title="Your Top Tracks"
+              items={top_tracks.items.slice(0, 10)}
+            />
+          )}
+
+          {top_artists.items.length > 1 && (
+            <ChartCard
+              title="Your Top Genres"
+              subtitle="Based on your top artists"
+            >
+              <GenreChart artists={top_artists.items} />
+            </ChartCard>
+          )}
+        </div>
+      </>
     );
   }
 
